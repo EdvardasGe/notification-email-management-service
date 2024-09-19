58978b481b75e80fc25892abada8995969bf7bf3
EdvardasGe <edvardasgervaitis@gmail.com>
2024-09-19 12:09:46 +0300

    Create notification service, saves notifications and sends

---
+++ src/main/java/com/edvardas/notificationservice/service/NotificationService.java
@@ -0,0 +1,25 @@
+package com.edvardas.notificationservice.service;
+
+import com.edvardas.notificationservice.model.Notification;
+import com.edvardas.notificationservice.repository.NotificationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class NotificationService {
+
+    @Autowired
+    private NotificationRepository notificationRepository;
+
+    @Autowired
+    private EmailService emailService;
+
+    public void sendNotification(String recipientEmail, String subject, String notificationBody) {
+        Notification notification = new Notification(subject, notificationBody, recipientEmail, LocalDateTime.now());
+        notificationRepository.save(notification);
+
+        emailService.sendSimpleEmail(recipientEmail, "New Notification", notificationBody);
+    }
+}
